e2f194ce326e47a2140aa362b7f4b25826da08ed
Kelenjo <giorgigioroblox13@gmail.com>
2025-09-28 19:27:03 +0400

    updated admin

--- pythonFoodOrder/src/admin_views/product.py
+++ pythonFoodOrder/src/admin_views/product.py
@@ -16,18 +16,64 @@ class ProductView(SecureModelView):
     create_modal = True
     edit_modal = True
 
+    column_list = ["name", "price", "categories.name", "image", "description"]
     column_editable_list = ("name", "price", "description")
-    column_filters = ["price",]
+    column_filters = ["price", "name", "categories.name"]
 
     column_formatters = {
-        "name": lambda v,c,m,n: m.name if len(m.name) < 24 else m.name[0:24] + "...",
-        "image": lambda v,c,m,n: Markup(f"<img src='/static/assets/{m.image}' width=64/>")
+        "name": lambda v, c, m, n: m.name if len(m.name) < 24 else m.name[:24] + "...",
+        "image": lambda v, c, m, n: Markup(f"""
+            <a href='/static/assets/{m.image}' target='_blank'>
+                <img src='/static/assets/{m.image}' width=80 style='border-radius:6px'/>
+            </a>
+        """)
     }
 
+    form_columns = ['name', 'description', 'price', "category_id", 'image']
     form_overrides = {"image": ImageUploadField}
     form_args = {
         "image": {
             "base_path": Config.UPLOAD_PATH,
-            "namegen": generate_filename
+            "namegen": generate_filename,
+            "allow_overwrite": False
         }
     }
+    form_ajax_refs = {
+        'categories': {'fields': ['name']}
+    }
+
+
+class CategoryView(SecureModelView):
+    create_modal = True
+    edit_modal = True
+    column_list = ["name"]
+    column_searchable_list = ["name"]
+    column_editable_list = ["name"]
+
+
+class OfferView(SecureModelView):
+    create_modal = True
+    edit_modal = True
+    column_list = ["title", "description", "active", "image"]
+    column_editable_list = ["title", "description", "active"]
+    column_formatters = {
+        "image": lambda v,c,m,n: Markup(f"""
+            <a href='/static/assets/{m.image}' target='_blank'>
+                <img src='/static/assets/{m.image}' width=80 style='border-radius:6px'/>
+            </a>
+        """)
+    }
+    form_overrides = {"image": ImageUploadField}
+    form_args = {
+        "image": {
+            "base_path": Config.UPLOAD_PATH,
+            "namegen": generate_filename,
+            "allow_overwrite": False
+        }
+    }
+
+
+class CartView(SecureModelView):
+    column_list = ["user_id", "product_id", "quantity"]
+    column_filters = ["user_id", "product_id"]
+    form_columns = ["user_id", "product_id", "quantity"]
